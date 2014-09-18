f0d0092afdd7ba3b9de5b30d0fb6c178cc227a18
Akshay Ramesh <akshay91r@gmail.com>
2014-09-18 18:00:28 -0400

    Initial Commit project setup

---
+++ Blowout/Assets/Scripts/KeyboardMovePlayer.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KeyboardMovePlayer : MonoBehaviour {
+
+	private float speed = 1.5f;
+	private float spacing = 1.0f;
+	private Vector2 pos;
+	
+	void Start() 
+	{
+		pos.x = transform.position.x;
+		pos.y = transform.position.y;
+	}
+	
+	void Update() 
+	{
+		if (Input.GetKeyDown(KeyCode.W))
+			rigidbody2D.AddForce(Vector3.up * 100 * Time.deltaTime);
+		/*if (Input.GetKeyDown(KeyCode.S))
+			pos.y -= spacing;
+		if (Input.GetKeyDown(KeyCode.A))
+			pos.x -= spacing;
+		if (Input.GetKeyDown(KeyCode.D))
+			pos.x += spacing;
+		
+		transform.position = Vector2.MoveTowards(transform.position, pos, speed * Time.deltaTime);*/
+	}
+}

cd84776f0e62a8f360f32d08f74548c1fd4a436b
unknown <jhagen@ETC-JHAGEN.andrew.ad.cmu.edu>
2014-09-22 00:39:30 -0400

    Added credits

---
+++ Blowout/Assets/Scripts/Credits.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Credits : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		((MovieTexture)(GameObject.FindGameObjectWithTag ("Finish").renderer.material.mainTexture)).Play ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
